782b553530227fcc8f12c8de5b9a954e955da37f
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2017-12-10 22:35:39 +0545

    Find acceleration using this program.

---
+++ acceleration.py
@@ -0,0 +1,8 @@
+v = float(input("Enter the value of velocity: "))
+#v = 25
+u = float(input("Enter the value of speed: "))
+#u = 0
+t = float(input("Enter the time taken: "))
+#t = 10
+a = (v-u)/t
+print("The acceleration is: {0:.3f}".format(a))
